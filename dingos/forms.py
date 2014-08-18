8425d4b1c46e055d71dd2e1fd966c1551e1d7854
Quentin <bernd.grobauer@siemens.com>
2014-08-18 11:23:00 +0200

    pagination 0 leads to problems, so we do 1000000.

--- dingos/forms.py
+++ dingos/forms.py
@@ -40,7 +40,7 @@ class EditSavedSearchesForm(forms.Form):
 class CustomQueryForm(forms.Form):
     query = forms.CharField(required=False,widget=widgets.Textarea(attrs={'cols':100,'rows':10,'style': 'height:auto; width:auto;'}))
     _choices = [(str(x), str(x)) for x in [50,100,200,300,400,500,1000,2]]
-    _choices.append(('0','ALL (DANGER!)'))
+    _choices.append(('1000000','ALL (DANGER!)'))
     paginate_by = forms.ChoiceField(choices=_choices,required=False,initial='100')
     page = forms.IntegerField(required=False,initial=1,widget=forms.HiddenInput)
 
