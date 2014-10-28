5fecf56633a1b88a10c2760f4b26906c17d22174
Bernd Grobauer <bernd.grobauer@siemens.com>
2014-10-28 11:33:42 +0100

    Placing SQL statements in migration.

--- dingos/migrations/0002_add_stored_function.py
+++ dingos/migrations/0002_add_stored_function.py
@@ -4,22 +4,27 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+create_build_graph = """-- Function build_graph(int[], string)
+-- returns a serialized graph object
+
+CREATE FUNCTION build_graph(pkslist int[], direction text, depth int)
+RETURNS text
+AS $$
+from dingos.core.db_graphtools import build_graph
+return build_graph(pkslist, direction, depth, plpy)
+$$ LANGUAGE plpythonu"""
+
+remove_build_graph = """DROP FUNCTION build_graph(int[], text, int)"""
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ('dingos', '0001_initial'),
     ]
 
-    fd = open('/home/mantis/ti/django-dingos/dingos/sql/build_graph.sql', 'r')
-    sql = fd.read()
-    fd = open('/home/mantis/ti/django-dingos/dingos/sql/build_graph_undo.sql', 'r')
-    undo = fd.read()
-    fd.close()
-
-
     operations = [
         migrations.RunSQL(
-            sql,
-            undo
+            create_build_graph,
+            remove_build_graph
         ),
     ]
