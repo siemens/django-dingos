60572af822ea41a56122fe7276399e24e8c57ec9
Philipp Lang <mantis.cert@siemens.com>
2015-02-11 13:22:08 +0100

    refactored show_TagDisplay.

--- dingos/templatetags/dingos_tags.py
+++ dingos/templatetags/dingos_tags.py
@@ -728,7 +728,7 @@ def show_InfoObjectTagRowDisplay(context, object, col_count, isEditable=False):
     }
 
 @register.inclusion_tag('dingos/%s/includes/_TagDisplay.html'% DINGOS_TEMPLATE_FAMILY)
-def show_TagDisplay(tags, obj_id, tag_type, isEditable = False):
+def show_TagDisplay(tags, tag_type, isEditable = False):
     context = {}
     if isinstance(tags,basestring):
         context['tags'] = [tags]
@@ -742,7 +742,7 @@ def show_TagDisplay(tags, obj_id, tag_type, isEditable = False):
         context['view_url'] = ''
 
     context['isEditable'] = isEditable
-    context['tagged_obj_id'] = obj_id
+    #context['tagged_obj_id'] = obj_id
     context['tag_type'] = tag_type
     return context
 
